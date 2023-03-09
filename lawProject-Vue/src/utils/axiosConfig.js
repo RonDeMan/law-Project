b2ac379cf47421d7bffdb001d2484d417465f7b5
rondeman <ron@ben-israel.com>
2023-03-09 17:39:30 +0200

    added amplify

--- lawProject-Vue/src/utils/axiosConfig.js
+++ lawProject-Vue/src/utils/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// https://ou6jqqyza5.execute-api.eu-west-3.amazonaws.com/dev
 const baseURL = process.env.NODE_ENV === "development"
 ? "http://localhost:4000/"
 : "http://lawproject.ben-israel.com/"
