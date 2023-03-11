d28fb6277e72f76d2d74637c0cc154e9b95318fc
rondeman <ron@ben-israel.com>
2023-03-11 19:40:59 +0200

    again

--- lawProject-Vue/amplify/backend/function/getQuestions/src/index.js
+++ lawProject-Vue/amplify/backend/function/getQuestions/src/index.js
@@ -7,17 +7,23 @@
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     const fs = require(`fs`)
-    return await fs.readFile("./jsons/questions.json", "utf8",(err, questions)=>{
+
+    let questions
+
+    await fs.readFile(".../jsons/questions.json", "utf8",(err, questionsJson)=>{
+        console.log(err)
         console.log(questions)
-        return {
-            statusCode: 200,
-        //  Uncomment below to enable CORS requests
-        //  headers: {
-        //      "Access-Control-Allow-Origin": "*",
-        //      "Access-Control-Allow-Headers": "*"
-        //  }, 
-            body: JSON.stringify(questions),
-        };
+        questions= questionsJson
     })
+    console.log(questions)
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify(questions),
+    };
     
 };
