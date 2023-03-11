0efaf0c54196ca1d176747cba346c469a5cff7bc
rondeman <ron@ben-israel.com>
2023-03-11 19:24:11 +0200

    fix

--- lawProject-Vue/amplify/backend/function/getQuestions/src/index.js
+++ lawProject-Vue/amplify/backend/function/getQuestions/src/index.js
@@ -8,6 +8,7 @@ exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     const fs = require(`fs`)
     return await fs.readFile("./jsons/questions.json", "utf8",(err, questions)=>{
+        console.log(questions)
         return {
             statusCode: 200,
         //  Uncomment below to enable CORS requests
