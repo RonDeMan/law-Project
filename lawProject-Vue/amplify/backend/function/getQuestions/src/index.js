bec62c56386aca1bc22db273b99971e10c6c43e5
rondeman <ron@ben-israel.com>
2023-03-17 13:25:48 +0200

    maybe?

--- lawProject-Vue/amplify/backend/function/getQuestions/src/index.js
+++ lawProject-Vue/amplify/backend/function/getQuestions/src/index.js
@@ -3,9 +3,10 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-import { Storage } from 'aws-amplify';
 
 exports.handler = async (event) => {
+    const { Storage } = require('@aws-amplify/storage');
+
     console.log(`EVENT: ${JSON.stringify(event)}`);
     const fs = require(`fs`)
 
